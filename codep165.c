aea0a444ae0936ae7de37188736f44ab2b04a13d
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-03-25 05:51:25 +0530

    Create codep165.c

---
+++ codep165.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+     int a[100],N,i,K,f=0,b;
+    scanf("%d %d",&N,&K);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+       
+            if(a[i]>K)
+            {
+                b=a[i];
+                f=0;
+            }
+        
+    }
+    if(f==0)
+    {
+        printf("%d",b);
+    }
+        
+   return 0;
+}
